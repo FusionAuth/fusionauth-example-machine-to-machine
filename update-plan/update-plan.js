6414202e5a174c244983b6d7dec2361e03c77df6
Dan Moore <dan@fusionauth.io>
2025-03-20 13:07:11 -0600

    added update of plan

---
+++ update-plan/update-plan.js
@@ -0,0 +1,32 @@
+import {FusionAuthClient} from '@fusionauth/typescript-client';
+import 'dotenv/config'
+
+const FUSIONAUTH_API_KEY = process.env.FUSIONAUTH_API_KEY;
+const BASE_URL = process.env.BASE_URL;
+const RECIPIENT_ENTITY_ID = process.env.RECIPIENT_ENTITY_ID;
+const TARGET_ENTITY_ID = process.env.TARGET_ENTITY_ID;
+const PERMISSIONS = ['news','weather'];
+
+const client = new FusionAuthClient(FUSIONAUTH_API_KEY, BASE_URL);
+
+async function grantPermission(entityId) {
+  try {
+    const req = {
+      grant: {
+        permissions: PERMISSIONS,
+        recipientEntityId: entityId
+      }
+    }
+    const response = await client.upsertEntityGrant(TARGET_ENTITY_ID, req);
+    
+    console.log('Permission granted:', response);
+  } catch (error) {
+    console.error('Error granting permission:', JSON.stringify(error));
+  }
+}
+
+(async () => {
+  await grantPermission(RECIPIENT_ENTITY_ID);
+})();
+
+
