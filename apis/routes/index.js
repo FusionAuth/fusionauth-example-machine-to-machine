bb442bcceaea8c566589180870364d49a64f7873
Dan Moore <dan@fusionauth.io>
2025-04-01 15:44:53 -0600

    extract api entity id to env var

--- apis/routes/index.js
+++ apis/routes/index.js
@@ -2,15 +2,18 @@ var express = require('express');
 var router = express.Router();
 const hasPermission = require('../services/hasPermission');
 
+require('dotenv/config');
+const API_ENTITY_ID = process.env.API_ENTITY_ID;
+
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/api/news', hasPermission('951a9109-7201-4f79-beaf-f7791097bdb2','news'), function (req, res, next) {
+router.get('/api/news', hasPermission(API_ENTITY_ID,'news'), function (req, res, next) {
   res.json({ news: [ { title: "news item", description: "news description" }]});
 });
 
-router.get('/api/weather', hasPermission('951a9109-7201-4f79-beaf-f7791097bdb2','weather'), function (req, res, next) {
+router.get('/api/weather', hasPermission(API_ENTITY_ID,'weather'), function (req, res, next) {
   res.json({ weather: { description: "weather description", high: 30, low: 10 }});
 });
 
