0fb8d7f17b9cc7d826a410bbd979dfca6b526c67
Dan Moore <dan@fusionauth.io>
2025-04-01 10:45:09 -0600

    built out example of incorrect request.

--- request-access-token/request-access-token.js
+++ request-access-token/request-access-token.js
@@ -7,21 +7,41 @@ const RECIPIENT_ENTITY_ID = process.env.RECIPIENT_ENTITY_ID;
 const RECIPIENT_ENTITY_SECRET = process.env.RECIPIENT_ENTITY_SECRET;
 const TARGET_ENTITY_ID = process.env.TARGET_ENTITY_ID;
 const PERMISSION = 'news';
+const INVALID_PERMISSION = 'weather';
 
 const client = new FusionAuthClient(FUSIONAUTH_API_KEY, BASE_URL);
 
-async function requestAccessToken() {
+function buildScope(targetEntityId, permissionString) {
+  return 'target-entity:'+targetEntityId+':'+permissionString;
+}
+
+async function requestAccessTokenSuccess() {
+  let access_token = null;
   try {
-    const response = await client.clientCredentialsGrant(RECIPIENT_ENTITY_ID, RECIPIENT_ENTITY_SECRET, PERMISSION); 
-    console.log('response: ', response);
-    console.log(response.response.access_token);
+    const response = await client.clientCredentialsGrant(RECIPIENT_ENTITY_ID, RECIPIENT_ENTITY_SECRET, buildScope(TARGET_ENTITY_ID,PERMISSION)); 
+    access_token = response.response.access_token;
   } catch (error) {
     console.error('Error getting access token:', JSON.stringify(error));
   }
+  return access_token;
+}
+
+async function requestAccessTokenFailure() {
+  let access_token = null;
+  try {
+    const response = await client.clientCredentialsGrant(RECIPIENT_ENTITY_ID, RECIPIENT_ENTITY_SECRET, buildScope(TARGET_ENTITY_ID,INVALID_PERMISSION)); 
+    access_token = response.response.access_token;
+  } catch (error) {
+    console.error('Error getting access token:', JSON.stringify(error));
+  }
+  return access_token;
 }
 
 (async () => {
-  await requestAccessToken();
+  let access_token = await requestAccessTokenSuccess();
+  console.log(access_token);
+  access_token = await requestAccessTokenFailure();
+  console.log(access_token);
 })();
 
 
